bd7ff5830d7f383d618e317cf47af5a4a1f8cd59
Kate Daly <kate.s.daly@gmail.com>
2014-04-05 17:30:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quinn::Application.config.secret_key_base = '789e7963efda05b54fe9b8c854380b9a6a6a07f549cf64f3fe13e045cbf3db8a9efd9b147e454017d0efa32f4b47bc9b8f88209d580479317894802958be391f'
