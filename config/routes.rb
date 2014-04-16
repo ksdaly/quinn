9506f1c023c8701922b50d1627c0866099ba2c5f
Kate Daly <kate.s.daly@gmail.com>
2014-04-15 22:12:15 -0400

    Mandrill setup

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Quinn::Application.routes.draw do
   resources :posts
 
   # mount_griddler
-  get '/email_processor', :to => proc {|env| [200, {}, ["OK"]] }
+  get "/email_processor", to: proc { [200, {}, ["OK"]] }, as: "mandrill_head_test_request"
 
-  # mount_griddler
 end
